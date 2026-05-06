c9aae8dbef5095ec76279f77d9d4a4f0658cb828
magnusneumann <magnusn2021@gmail.com>
2026-05-06 13:40:31 +0200

    gutes Lanefollowing

--- launchers/follow_lane.sh
+++ launchers/follow_lane.sh
@@ -3,8 +3,10 @@ source /opt/ros/noetic/setup.bash
 source devel/setup.bash
 
 rosrun follow_lane detect_lane_node.py &
+rosrun follow_lane configuration_node.py &
 rosrun follow_lane switch_control_node.py &
 sleep 5
 
-rosrun follow_lane control_lane_node.py &
+rosrun follow_lane control_lane_node.py
+
 wait
